cfaf231261ef65402b81c31aa74fe3a637b890c0
Vinesma <otaviocos_13@hotmail.com>
2020-12-16 19:36:17 -0300

    Add more stuff and a README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Currículo / Resume
+
+[PT/BR] Esse repositório contém templates para gerar meu currículo em Português e Inglês.
+
+[EN] This repository contains templates to generate my resume in both Portuguese and English.
+
+## Licences
+
+The icons used are property of [Font Awesome.](https://fontawesome.com/license) Nothing was changed from the original files.
+
--- render.py
+++ render.py
@@ -1,8 +1,14 @@
 from jinja2 import FileSystemLoader, Environment
-import json
+from time import localtime
+import json, subprocess
 
 with open("./data/ptbr.json") as dataFile:
     data = json.load(dataFile)
+
+year, month, day = localtime()[:3]
+name = data['nome']
+filename_pdf_pt = f"Curriculo - {name} {day}-{month}-{year}.pdf"
+filename_pdf_en = f"Resume - {name} {day}-{month}-{year}.pdf"
 
 def renderFromTemplate(directory, template_name, **kwargs):
     loader = FileSystemLoader(directory)
@@ -12,3 +18,6 @@ def renderFromTemplate(directory, template_name, **kwargs):
 
 with open("resume.html", "w") as resume:
     resume.write(renderFromTemplate("./template", "template1.html", **data))
+
+# Convert to pdf
+subprocess.run(["wkhtmltopdf", "--enable-local-file-access", "-B", "0", "-L", "0", "-R", "0", "-T", "0", "resume.html", filename_pdf_pt], check=True)
